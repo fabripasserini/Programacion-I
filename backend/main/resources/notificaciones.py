bf0484e5f1863c0f4a48f056fe1ca7384f3f2378
Eyudica <e.yudica@alumno.um.edu.ar>
2025-04-08 16:04:22 -0300

    Modify resources

--- backend/main/resources/notificaciones.py
+++ backend/main/resources/notificaciones.py
@@ -1,9 +1,16 @@
 from flask_restful import Resource
 from flask import request
 
-notificaciones = {}
+notificaciones = {
+    1:{'destinatario':'juan','mensaje':'hola'},
+    2:{'destinatario':'admin','mensaje':'hola2'}
+
+}
 
 class Notificacion(Resource):
+    def get(self):
+        return notificaciones,200
+    
     def post(self):
         data = request.get_json()
         id = max(notificaciones) + 1
